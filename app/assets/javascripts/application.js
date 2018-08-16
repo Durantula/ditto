731b6c1006c8990d9d0074c0ade0de74eb326cce
Thamer.AlMerry <thamer.almerry@webjet.com.au>
2018-08-16 15:40:45 +1000

    latest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,34 @@
 
 $(document).on('turbolinks:load', function() {
     codeMirrorDictionary = new Object();
+    
+/* Toggle Filters */
+$( "#filters" ).click(function() {
+    $('.fa-angle-down').toggleClass('active');
+    $( ".filters" ).slideToggle( "slow", function() {
+      $(this).toggleClass('collapsed');
+    });
+  });
 
+/* Range Slider */
+    var rangeSlider = function(){
+        var slider = $('.range-slider'),
+            range = $('.range-slider__range'),
+            value = $('.range-slider__value');
+          
+        slider.each(function(){
+      
+          value.each(function(){
+            var value = $(this).prev().attr('value');
+            $(this).html('> ' +value + 'k');
+          });
+      
+          range.on('input', function(){
+            $(this).next(value).html('> ' +this.value + 'k');
+          });
+        });
+      };
+      
+      rangeSlider();    
 
-}
+});
